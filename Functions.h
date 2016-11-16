5d0b03c846cee5907616086b0aed0805385a70c6
Joel Dsouza <shadowk97@gmail.com>
2016-11-16 18:43:14 +0530

    Create Functions.h

---
+++ Functions.h
@@ -0,0 +1,29 @@
+#ifndef FUNCTIONS_H
+#define FUNCTIONS_H
+
+
+
+
+class Functions
+{
+    public:
+        float one, two;
+        int x;
+        float height, base;
+        float area;
+
+
+        Functions();
+        void Adder();
+        void subtractor();
+        void divider();
+        void multiplier();
+        void shutdown();
+        void Random();
+        void IpAdd();
+        void DateTime();
+        void AreaOfTriangle();
+};
+
+
+#endif // FUNCTIONS_H
